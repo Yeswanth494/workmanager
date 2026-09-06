b6b0f164d5dae788c428a2290d5261e45656b187
Srinivasa yeswanth <yeswanth.kona999@gmail.com>
2026-09-06 11:13:50 +0530

    Allow Vercel frontend in CORS

--- backend/src/main/java/com/sri/worksphere/config/SecurityConfig.java
+++ backend/src/main/java/com/sri/worksphere/config/SecurityConfig.java
@@ -45,9 +45,10 @@ public class SecurityConfig {
         CorsConfiguration configuration =
                 new CorsConfiguration();
 
-        configuration.setAllowedOrigins(
-                List.of("http://localhost:5173")
-        );
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://workmanager-srinivasa1.vercel.app"
+        ));
 
         configuration.setAllowedMethods(
                 List.of(
